8ff96598655d5e4c83bd9f5bcf2f2ef3cb5c41e2
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2019-10-04 15:58:29 -0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,2 +1,2 @@
 alias flutter="/workspace/FlutterUnicampMiniCurso/flutter/bin/flutter"
-git clone https://github.com/BrunoEleodoro/FlutterTwitterClone
+git clone https://github.com/BrunoEleodoro/FlutterTwitterClone --branch start
